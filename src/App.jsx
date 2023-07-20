7f3bb67d20f5d7384e627fa88527497afb2c0af4
Gonzalo <gonzalomorresi@gmail.com>
2023-07-20 17:08:36 -0300

    saco el hola pepe

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,6 @@ function App() {
   }, []);
   return (
     <Box>
-      <p>Hola pepe</p>
       <CartContext.Provider value={{ cartState, setCartState }}>
         <StickyCart />
         <RouterProvider router={router} />
